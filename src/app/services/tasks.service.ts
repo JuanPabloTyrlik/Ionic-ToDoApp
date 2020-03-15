9086e242737997d8e5fc16ed3aaeb31dfed17c38
Juan Pablo Tyrlik <juan.pablo.tyrlik@gmail.com>
2020-03-15 19:25:29 -0300

    created testlists, added items on tab1, and changed styles

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -6,5 +6,12 @@ import { List } from '../models/list.model';
 })
 export class TasksService {
 
-  constructor() {}
+  list: List[] = [];
+
+  constructor() {
+    const list1 = new List('Test List 1');
+    const list2 = new List('Test List 2');
+
+    this.list.push(list1, list2);
+  }
 }
