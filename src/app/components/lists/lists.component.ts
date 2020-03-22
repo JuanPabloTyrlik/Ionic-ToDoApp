c3c4c9faf85236a577dcd1356b057cafde062517
Juan Pablo Tyrlik <juan.pablo.tyrlik@gmail.com>
2020-03-22 14:59:23 -0300

    Rename list

--- src/app/components/lists/lists.component.ts
+++ src/app/components/lists/lists.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { TasksService } from '../../services/tasks.service';
 import { Router } from '@angular/router';
 import { List } from '../../models/list.model';
+import { AlertController, IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-lists',
@@ -10,10 +11,12 @@ import { List } from '../../models/list.model';
 })
 export class ListsComponent implements OnInit {
 
+  @ViewChild(IonList, {static: false}) slidingList: IonList;
   @Input() pending = true;
 
   constructor(  public tasksService: TasksService,
-                private router: Router  ) {}
+                private router: Router,
+                private alert: AlertController  ) {}
 
   ngOnInit() {}
 
@@ -27,5 +30,40 @@ export class ListsComponent implements OnInit {
 
   deleteList(list: List) {
     this.tasksService.deleteList(list);
+  }
+
+  async renameList(list: List) {
+    const alert = await this.alert.create({
+      header: 'Modificar nombre de la lista',
+      inputs: [
+        {
+          name: 'title',
+          type: 'text',
+          value: list.title,
+          placeholder: 'Nombre de la lista'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => this.slidingList.closeSlidingItems()
+        },
+        {
+          text: 'Modificar',
+          role: 'submit',
+          handler: ( data ) => {
+            if (data.title.length === 0) {
+              return;
+            }
+            list.title = data.title;
+            this.tasksService.saveLocalStorage();
+            this.slidingList.closeSlidingItems();
+          }
+        }
+      ]
+  });
+
+    await alert.present();
   }
 }
